135a48303e5f75fa68a7ab816149f81ce2ac3e98
Zack Wins <el_hefe@Zacks-MBP.attlocal.net>
2020-11-01 15:31:07 -0500

    chat 2 is in good working state

--- src/views/Chat2.vue
+++ src/views/Chat2.vue
@@ -1,7 +1,8 @@
 <template>
   <Suspense>
     <template #default>
-      <div class="flex h-screen bg-gray-200 font-roboto">
+      <div :class="getClass('preSidebar')">
+        <!-- Sidebar -->
         <div class="flex">
           <!-- Backdrop -->
           <div
@@ -51,60 +52,179 @@
             </nav>
           </div>
         </div>
-
-        <div class="w-2/4">
-          <div v-if="loading"><LoadingPanel /></div>
-          <div class=" w-full">
-            <h3>
-              {{
-                currentChatThread ? currentChatThread.title : "Select A Chat"
-              }}
-            </h3>
-            <button @click.prevent="addMyProfileToThread">
-              Add My Profile
-            </button>
-            <hr />
-            <div
-              id="messageContainer"
-              class="overflow-y-auto"
-              style="max-height:600px;"
-            >
-              <div v-for="m in reverseMessages" :key="m.date">
-                <div class="items-start pb-4 bg-gray-100 rounded">
-                  <img
-                    :src="getProfileImageForMessage(m)"
-                    class="w-10 h-10 rounded mr-3"
+        <!-- End Sidebar -->
+        <!-- Begin Header -->
+        <div :class="getClass('preHeader')">
+          <header
+            class="flex justify-between items-center py-4 px-6 bg-white border-b-4 border-indigo-600"
+          >
+            <div class="flex items-center">
+              <button
+                @click="isOpen = true"
+                class="text-gray-500 focus:outline-none lg:hidden"
+              >
+                <svg
+                  class="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4 6H20M4 12H20M4 18H11"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
                   />
-                  <div class="flex-1 overflow-hidden">
-                    <div class="">
-                      <span class="font-bold text-xs underline">{{
-                        getDisplayNameForMessage(m)
-                      }}</span>
-                      <span class="pl-3"></span>
-                      <span class="text-grey text-xs"> </span>
+                </svg>
+              </button>
+
+              <div class="relative mx-4 lg:mx-0">
+                <div class="text-3xl">{{ currentChatThread.title }}</div>
+                <!-- <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
+          <svg class="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
+            <path
+              d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </span> -->
+
+                <!-- <input
+          class="form-input w-32 sm:w-64 rounded-md pl-10 pr-4 focus:border-indigo-600"
+          type="text"
+          placeholder="Search"
+        /> -->
+              </div>
+            </div>
+
+            <div class="flex items-center">
+              <div>
+                <span class="rounded px-1 py-1" :class="network">
+                  {{ network }}
+                </span>
+              </div>
+              <div>
+                <span class="rounded px-1 py-1 ml-1" :class="chain">
+                  {{ chain }}
+                </span>
+              </div>
+              <button class="flex mx-4 text-gray-600 focus:outline-none">
+                <svg
+                  class="h-6 w-6"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+              <div @click="dropdownOpen = !dropdownOpen">{{ profile ? profile.displayName : displayName  }}</div>
+              <div class="relative">
+                <!-- <button
+          @click="dropdownOpen = !dropdownOpen"
+          class="relative z-10 block h-8 w-8 rounded-full overflow-hidden shadow focus:outline-none"
+        >
+          <img
+            class="h-full w-full object-cover"
+            src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=296&q=80"
+            alt="Your avatar"
+          />
+        </button> -->
+                <div
+                  v-show="dropdownOpen"
+                  @click="dropdownOpen = false"
+                  class="fixed inset-0 h-full w-full z-10"
+                ></div>
+
+                <div
+                  v-show="dropdownOpen"
+                  class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20"
+                >
+                  <a
+                    href="/profile"
+                    class="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white"
+                  >
+                    Profile
+                  </a>
+                  <button
+                    @click="logout"
+                    class="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white"
+                  >
+                    Log out
+                  </button>
+                </div>
+              </div>
+            </div>
+          </header>
+          <!-- End Header -->
+          <main :class="getClass('main')">
+            <div :class="getClass('content')">
+              <div class='flex'>
+                <div class="w-full md:w-3/4">
+                  <div v-if="loading"><LoadingPanel /></div>
+                  <div class=" w-full flex-reverse">
+                    <div
+                      id="messageContainer"
+                      class="overflow-y-auto"
+                      style="max-height:600px;"
+                    >
+                      <div v-for="m in reverseMessages" :key="m.date">
+                        <div class="items-start pb-4 bg-gray-100 rounded">
+                          <img
+                            :src="getProfileImageForMessage(m)"
+                            class="w-10 h-10 rounded mr-3"
+                          />
+                          <div class="flex-1 overflow-hidden">
+                            <div class="">
+                              <span class="font-bold text-xs underline">{{
+                                getDisplayNameForMessage(m)
+                              }}</span>
+                              <span class="pl-3"></span>
+                              <span class="text-grey text-xs"> </span>
+                            </div>
+                            <br />
+                            <div class="message">
+                              <vue3-markdown-it :source="JSON.parse(m).message" />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                    <br />
-                    <div class="message">
-                      <vue3-markdown-it :source="JSON.parse(m).message" />
+                  </div>
+                  <NewMessage />
+                </div>
+                <div class="hidden md:block md:w-1/4 pl-2 pr-2 ">
+                <button class='pt-1 pb-1 mx-auto rounded btn-primary w-full' @click.prevent="addMyProfileToThread">
+                      Add My Profile
+                    </button>
+                  <h3 class="pt-2">Profiles In Chat</h3>
+                  
+                  <div v-for="profile in profiles" :key="profile._rev">
+                    <Sidecard :profile="profile" />
+                  </div>
+                  <hr/>
+                  <br/>
+                  <h3>Lurkers</h3>
+                  <div v-for="owner in currentChatThread._owners" :key="owner">
+                    <div v-if="!hasProfile(owner)">
+                      <p class='break-all pt- pb-1'><i class="fas fa-key pl-2"></i>: {{ owner }}</p>
+                      <hr/>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <NewMessage />
-        </div>
-        <div class="w-1/4">
-          <h3>Members In Chat</h3>
-          <div v-for="profile in profiles" :key="profile._rev">
-            <Sidecard :profile="profile" />
-          </div>
-          <h3>Lurkers</h3>
-          <div v-for="owner in currentChatThread._owners" :key="owner">
-            <div v-if="!hasProfile(owner)">
-              {{ owner }}
-            </div>
-          </div>
+          </main>
         </div>
         <!-- Begin New Chat Modal -->
         <div
@@ -216,7 +336,11 @@
                     </button>
                     <button
                       @click.prevent="
-                        createNewChatThread(titleForNewChat, pkForNewChat, publicKey)
+                        createNewChatThread(
+                          titleForNewChat,
+                          pkForNewChat,
+                          publicKey
+                        )
                       "
                       class="px-3 py-1 bg-indigo-600 text-white rounded-md text-sm hover:bg-indigo-500 focus:outline-none"
                     >
@@ -264,6 +388,7 @@ export default {
     //Use const = Ref ->
     //When if a value change should notify the UI
     //causing an update
+    const dropdownOpen = ref(false);
     const loading = ref(false);
     const initLoaded = ref(false);
     const showNewChat = ref(false);
@@ -278,6 +403,7 @@ export default {
     const savedChatId = ref("");
     const titleForNewChat = ref("");
     const pkForNewChat = ref("");
+    const displayName = window.localStorage.getItem(LSConstants.DISPLAYNAME);
     const computer = new Computer({
       seed: seed,
       chain: chain,
@@ -479,6 +605,8 @@ export default {
     }
     console.log("Chat2.setup() returning");
     return {
+      dropdownOpen,
+      displayName,
       loading,
       updateLoading,
       seed,
@@ -663,7 +791,11 @@ export default {
           console.log("PubKey Profile Pair to Save:", pair);
           let result = await syncedForSend.addProfile(pair);
           console.log("The result of adding the profile was: ", result);
-          if(result === "success"){alert("Successfully Added Your Profile Refresh To See The Changes.")}
+          if (result === "success") {
+            alert(
+              "Successfully Added Your Profile Refresh To See The Changes."
+            );
+          }
         }
       } else {
         console.log("The chat thread does not have a profiles field.");
@@ -911,6 +1043,15 @@ export default {
       }
       this.pausePolling = false;
       this.showNewChat = false;
+    },
+    logout() {
+      console.log("clearing local storage");
+      window.localStorage.clear();
+      console.log(
+        "Display name should be null and is :",
+        window.localStorage.getItem(LSConstants.DISPLAYNAME)
+      );
+      this.$router.push("/");
     },
     getClass(placement) {
       let returnClass = "";
